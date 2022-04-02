5bc582b5ddf1aa3c05338f4d9f573542a858e671
Philip <philipmboyah@gmail.com>
2022-04-02 07:27:22 +0000

    initial commit

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+app.listen(process.env.PORT || 3000, function() {
+  console.log("sever is running on port 3000");
 });
